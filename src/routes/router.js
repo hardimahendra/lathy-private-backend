a46bff5674814e1b8156ef524af2a4f220ee5297
hardimahendra <hardimahendra35@gmail.com>
2024-02-12 19:44:37 +0700

    ready for production

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,17 +3,16 @@ import cors from 'cors';
 
 const router = express.Router();
 
-import testServer from '../controller/testServer.js';
+
 import { login, register, profile } from '../controller/UserController.js';
 
 router.use(
   cors({
+    origin: ['https://lathy-private.vercel.app'],
     credentials: true,
-    origin: 'http://localhost:5173',
   })
 );
 
-router.get('/', testServer);
 
 router.post('/register', register);
 router.post('/login', login);
