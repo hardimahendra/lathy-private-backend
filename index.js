5b47e95bbab399ca1fd4c6eb4e1f8a11284baff4
hardimahendra <hardimahendra35@gmail.com>
2024-02-13 23:44:17 +0700

    update setting vercel.json

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 dotenv.config();
 app.use(
   cors({
-    origin: ['https://lathy-private-backend.vercel.app'],
+    origin: ['https://lathy-private.vercel.app'],
     credentials: true,
   })
 );
